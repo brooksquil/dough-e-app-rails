8366d27e72c364bdaf208438e3c6be248fca4a43
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-12-27 18:37:30 -0600

    Fix Login/Logout & redirects

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,22 @@
 class SessionsController < ApplicationController
-    
-      def new
-      end
-    
-      def create
-        user = User.find_by_email(params[:email])
-        # If the user exists AND the password entered is correct.
-        if user && user.authenticate(params[:password])
-          # Save the user id inside the browser cookie. This is how we keep the user 
-          # logged in when they navigate around our website.
-          session[:user_id] = user.id
-          redirect_to '/'
-        else
-        # If user's login doesn't work, send them back to the login form.
-          redirect_to '/login'
-        end
-      end
-    
-      def destroy
-        session[:user_id] = nil
-        redirect_to '/login'
-      end
-    
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      flash.now.alert = 'Email or password is invalid'
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
 end
+
+    
